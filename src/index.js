bd1128de87cdde9f6b31da18592dfa040d4c6e99
kattrinkr <kattrinkr@mail.ru>
2018-02-27 01:14:34 +0300

    feature/count of zeroes(8)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+
+    var primary = [];
+    var tempbase = base;
+    var j = 0;
+    for (var i = 2; i <= base; i++)
+      if ((base % i) === 0) {
+        primary.push(i);
+        //j++;
+        base = Math.floor(base / i);
+      }
+
+
+    var zeroes = [];
+    for (i = 0; i < primary.length; i++){
+      zeroes.push(primary[i]);
+    }
+
+    for (i = 0; i < tempbase; i++){
+      for (j = 0; j < primary.length; j++){
+        if (((i * primary[j]) % 10) === 0)
+          zeroes[j]++;
+      }
+    }
+    var newbase = 0;
+    if (primary.length === 1)
+    {newbase = primary[0];}
+      else
+      {for (i = 0; i < primary.length-1; i++)
+        if (primary[i] > primary[i+1]){
+        newbase = primary[i];}
+        else newbase = primary[i+1];}
+      var answer = 0;
+      while (number >= newbase) {
+      number = Math.floor(number / newbase);
+      answer = answer + number;
+
+}
+
+  return answer;
 }
